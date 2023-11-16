1e669b33648edc4bc8e1bd5fce84fbfac7028d6e
melkybahia88@gmail.com <melkybahia88@gmail.com>
2023-11-15 22:18:06 -0300

    Visual update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     var botaoTopo = document.getElementById("r-button");
     var textoTopo = document.getElementById("r-text");
-  
+    botaoTopo.style.display = "none";
+
     window.addEventListener("scroll", function () {
-      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
         botaoTopo.style.display = "block";
       } else {
         botaoTopo.style.display = "none";
