30ca5d54039ffff8fe430f0499e5e082b76f3b11
MelkysedekeDev <melkybahia88@gmail.com>
2023-11-15 21:12:29 -0300

    Pushing project

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var botaoTopo = document.getElementById("r-button");
+    var textoTopo = document.getElementById("r-text");
+  
+    window.addEventListener("scroll", function () {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        botaoTopo.style.display = "block";
+      } else {
+        botaoTopo.style.display = "none";
+      }
+    });
+
+    window.addEventListener("scroll", function () {
+    var alturaPagina = document.body.scrollHeight - this.window.innerHeight;
+
+        if (window.scrollY > alturaPagina - 1 && window.innerWidth > 1340) {
+          textoTopo.style.display = "block";
+        } else {
+          textoTopo.style.display = "none";
+        } 
+      });
+
+    window.addEventListener("resize", function () {
+      if (window.innerWidth > 1340) {
+        textoTopo.style.display = "block";
+      } else {
+        textoTopo.style.display = "none";
+      }
+    })
+  
+    botaoTopo.addEventListener("click", function () {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    });
+  });
