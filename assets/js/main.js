5df30dec0f5d757a7de5c1f1d7002a92f2640860
melkybahia88@gmail.com <melkybahia88@gmail.com>
2023-11-30 23:06:02 -0300

    Adding functionality to buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btnMobile = document.getElementById("btn-menu");
+    const btnQuemSomos = document.getElementById("btn-qs");
+    const btnNossasRedes = document.getElementById("btn-nr");
+    const btnMenu = document.getElementById('btn-menu');
+
+    function quemSomos (){
+        const elementoQuemSomos = document.getElementById('quem-somos');
+        elementoQuemSomos.scrollIntoView({ block: 'center', behavior: 'smooth'});
+    }
+
+    function nossasRedes (){
+        const elementoQuemSomos = document.getElementById('social-main');
+        elementoQuemSomos.scrollIntoView({ block: 'center', behavior: 'smooth'});
+    }
 
     function toggleMenu (event){
         if (event.type === 'touchstart') event.preventDefault();
@@ -8,6 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const active = nav.classList.contains('active');
     }
 
+    function menuAberto (){
+        document.body.classList.toggle('menu-aberto');
+    }
+
     btnMobile.addEventListener('click', toggleMenu);
     btnMobile.addEventListener('touchstart', toggleMenu);
+    btnQuemSomos.addEventListener('click', quemSomos);
+    btnNossasRedes.addEventListener('click', nossasRedes);
+    btnMenu.addEventListener('click', menuAberto);
 });
